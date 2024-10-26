c6eb519bba930cf3d9db497a699adc90a210e70a
Arthur Sousa <arthur.mdsousa@gmail.com>
2024-10-26 15:21:07 -0300

    spring boot project

---
+++ test.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+public class test {
+
+    public static void main(String[] args) {
+        List<String> palavras = new ArrayList<>();
+        palavras.add("salve");
+        palavras.add("fodasekkk");
+        palavras.add("eae bao");
+        
+        Collections.sort(palavras);
+        System.out.println(palavras);
+    }
+
+}
+    
